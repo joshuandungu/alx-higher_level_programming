a02145722c1b8a43628bb558c69a09bb6b6f8cb3
joshuandungu <joshuandungu2001@gmail.com>
2024-01-22 01:03:38 +0300

    0x03. Python - Data Structures: Lists, Tuples

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    tuple_a = check_tuple(tuple_a)
-    tuple_b = check_tuple(tuple_b)
+    tuple_a = validate_tuple(tuple_a)
+    tuple_b = validate_tuple(tuple_b)
 
     return ((tuple_a[0] + tuple_b[0]), (tuple_a[1] + tuple_b[1]))
 
 
-def check_tuple(_tuple=()):
+def validate_tuple(_tuple=()):
     if len(_tuple) < 2:
         if len(_tuple) == 1:
             _tuple = (_tuple[0], 0)
@@ -15,3 +15,4 @@ def check_tuple(_tuple=()):
     elif len(_tuple) > 2:
         _tuple = (_tuple[0], _tuple[1])
 
+    return _tuple
