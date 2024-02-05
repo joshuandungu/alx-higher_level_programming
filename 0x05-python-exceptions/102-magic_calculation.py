cd4634809351112236726a9e2878e514bc7fa268
joshuandungu <joshuandungu2001@gmail.com>
2024-02-06 00:18:21 +0300

    0x05-python-exception

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import dis
+
+
+def magic_calculation(a, b):
+    result = 0
+
+    for x in range(1, 3):
+        try:
+            if x > a:
+                raise Exception('Too far')
+
+            result += a ** b / x
+        except:
+            result = b + a
+            break
+
+    return result
